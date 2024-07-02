daf8e1fbfade67e2ccf0b032d61e623304b73e9a
Oleksii Sydorchuk <oleksii.sydorchuk@gmail.com>
2024-07-02 21:37:26 +0200

    added google tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@mui/material";
 import { myTheme } from "./_themes/themes";
 import { IsClientContextProvider } from "./_context/IsClientContextProvider";
 import { AuthProvider } from "./_context/AuthProvider";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const montserratFont = Montserrat({ subsets: ["latin", "cyrillic"] });
 
@@ -15,6 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+            <GoogleAnalytics gaId="AW-450448659" />
             <AuthProvider>
                 <IsClientContextProvider>
                     <ThemeProvider theme={myTheme}>
